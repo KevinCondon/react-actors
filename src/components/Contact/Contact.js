819b09771594fbb6cc728e7419fbe89874ee6c9c
Kevin Condon <kevincondon96@gmail.com>
2019-09-18 10:13:24 -0500

    test

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <>
+            <h2>Contact</h2>
+            <p>1-111-111-1111</p>
+            <p>TheyreAfterMe@Email.com</p>
+            </>
+            );
+    }
+}
+
+export default Contact;
